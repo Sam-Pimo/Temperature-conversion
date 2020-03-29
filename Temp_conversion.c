1897f109a1f706643106108775250246f6c878d3
Sam-Pimo <38015215+Sam-Pimo@users.noreply.github.com>
2020-03-29 09:41:12 +0300

    Update Temp_conversion.c

--- Temp_conversion.c
+++ Temp_conversion.c
@@ -2,6 +2,8 @@
 int main(){
 /*This converts Fahrenheit to celsius and displays the values in a somewhat tabular form.
 The program is inflexible but I'm working on it.*/
+  
+  //Fahrenheit-Celsius Table
 
   float fahrenheit, celsius;
   int first = 0;
@@ -14,6 +16,13 @@ The program is inflexible but I'm working on it.*/
     celsius = (5.0/9.0) * (fahrenheit - 32);
     printf("%3.0f %6.1f\n", fahrenheit, celsius); //adding width to the output.
     fahrenheit = fahrenheit + step;
+  }
+  
+  //Celsius-Fahrenheit Table
+  int fahrenheit;
+  printf("Celsius Fahr\n");
+  for(int fahrenheit = 0; fahrenheit <= 400; fahrenheit = fahrenheit + 20){
+    printf("%6.1f %3d\n", (5.0/9.0) * (fahrenheit - 32), fahrenheit );
   }
   return 0;
 }
